328de84d150e0444e47ab37e374fc3c11bd94b56
Kevin O'Keeffe <kevokeeffe@gmail.com>
2024-11-19 00:24:10 +0000

    refactor: renamed logs folder to activities

---
+++ OKeeffeCraft.Models/Activities/ActivityLogModel.cs
@@ -0,0 +1,15 @@
+﻿namespace OKeeffeCraft.Models.Logs
+{
+    public class ActivityLogModel
+    {
+        public string? Id { get; set; }
+
+        public DateTime? LogDate { get; set; }
+
+        public string? IdentifierType { get; set; }
+
+        public string? Identifier { get; set; }
+
+        public string? LogDetails { get; set; }
+    }
+}
---
+++ OKeeffeCraft.Models/Activities/ErrorLogModel.cs
@@ -0,0 +1,17 @@
+﻿namespace OKeeffeCraft.Models.Logs
+{
+    public class ErrorLogModel
+    {
+        public string? Id { get; set; }
+
+        public DateTime? LogDate { get; set; }
+
+        public string? IdentifierType { get; set; }
+
+        public string? Identifier { get; set; }
+
+        public string? LogDetails { get; set; }
+
+        public string? StackTrace { get; set; }
+    }
+}
